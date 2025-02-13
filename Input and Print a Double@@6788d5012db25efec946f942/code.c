c252bb948dd3035b473ba987eb55815def43ef92
choudhary890890 <choudharylakshya118@gmail.com>
2025-02-13 23:37:03 +0530

    Solution to Input and Print a Double

--- Input and Print a Double@@6788d5012db25efec946f942/code.c
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
 float a;
-printf(" enter a double precesion floating point number:");
+printf("enter a double precesion floating point number:");
 scanf("%1f",&a);
-printf("You entered: %1f\n", a);
+printf("You entered: %.f\n", a);
 }
