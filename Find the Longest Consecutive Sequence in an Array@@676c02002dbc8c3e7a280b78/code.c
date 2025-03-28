756d670238e49cab71fba50d186e5e067401011e
choudhary890890 <choudharylakshya118@gmail.com>
2025-03-28 20:54:58 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
- 
- void bubbleSort(int arr[], int n) {
-     int t;
-     for (int i = 0; i < n-1; i++) {
-         for (int j = 0; j < n-i-1; j++) {
-             if (arr[j] > arr[j+1]) {
-                 t = arr[j];
-                 arr[j] = arr[j+1];
-                 arr[j+1] = t;
-             }
-         }
-     }
- }
- 
- int main(){
-     int n,c;
-     c=0;
-     scanf("%d", &n);
-     int arr[n];
-     for(int i=0; i<n; i++){
-         scanf("%d", &arr[i]);
-     }
-     bubbleSort(arr,n);
-     int count=0, val=1;
-     int res[val];
-     for(int i=0; i<n; i++){
-         if(arr[i]==arr[i-1]+1){
-             count++;
-             if(count>val){
-                 val = count;
-             }
-         }
-         else{
-             count=1;
-         }
-     }
-     printf("%d",val);
-     printf("%d",arr[-1]);
-     return 0;
- }
+
+void bubbleSort(int arr[], int n) {
+    int t;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];  
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    bubbleSort(arr, n);  // Sorting the array
+
+    int maxSeq = 1, count = 1;
+
+    for (int i = 1; i < n; i++) {  // Start from index 1 to avoid out-of-bounds access
+        if (arr[i] == arr[i - 1] + 1) {  // Check consecutive sequence
+            count++;
+            if (count > maxSeq) {
+                maxSeq = count;
+            }
+        } else if (arr[i] != arr[i - 1]) {  // Reset count if it's not a duplicate
+            count = 1;
+        }
+    }
+
+    printf("%d\n", maxSeq);  // Print the longest consecutive sequence length
+    return 0;
+}
